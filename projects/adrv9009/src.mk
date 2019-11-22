d09d4f528e2d1f7117763b7abb3fbc426ef40edd
Cristian Pop <cristian.pop@analog.com>
2019-11-22 14:39:59 +0200

    iio: Add iio application.
    This instantiates "iio_axi_adc" and "iio_axi_dac" modules, that permits
    reading/writing and parametrization, of "axi_adc" and "axi_dac" devices.
    
    Signed-off-by: Cristian Pop <cristian.pop@analog.com>

--- projects/adrv9009/src.mk
+++ projects/adrv9009/src.mk
@@ -38,10 +38,17 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c			\
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c			\
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c			\
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c		\
+	$(NO-OS)/iio/iio.c						\
+	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.c				\
+	$(NO-OS)/iio/iio_axi_dac/iio_axi_dac.c				\
+	$(NO-OS)/util/fifo.c						\
+	$(NO-OS)/util/xml.c						\
 	$(NO-OS)/util/util.c
 SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
 	$(PLATFORM_DRIVERS)/spi.c					\
 	$(PLATFORM_DRIVERS)/gpio.c					\
+	$(PLATFORM_DRIVERS)/irq.c					\
+	$(PLATFORM_DRIVERS)/uart.c					\
 	$(PLATFORM_DRIVERS)/delay.c
 INCS :=	$(PROJECT)/src/app/app_config.h					\
 	$(PROJECT)/src/devices/ad9528/ad9528.h				\
@@ -87,10 +94,23 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h			\
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h			\
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h
 INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h					\
-	$(PLATFORM_DRIVERS)/gpio_extra.h
+	$(PLATFORM_DRIVERS)/gpio_extra.h				\
+	$(PLATFORM_DRIVERS)/irq_extra.h					\
+	$(PLATFORM_DRIVERS)/uart_extra.h
 INCS +=	$(INCLUDE)/axi_io.h						\
 	$(INCLUDE)/spi.h						\
 	$(INCLUDE)/gpio.h						\
 	$(INCLUDE)/error.h						\
 	$(INCLUDE)/delay.h						\
-	$(INCLUDE)/util.h
+	$(INCLUDE)/util.h						\
+	$(INCLUDE)/irq.h						\
+	$(INCLUDE)/uart.h						\
+	$(INCLUDE)/xml.h						\
+	$(INCLUDE)/fifo.h						\
+	$(INCLUDE)/util.h						\
+	$(NO-OS)/iio/iio.h						\
+	$(NO-OS)/iio/iio_types.h					\
+	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.h				\
+	$(NO-OS)/iio/iio_axi_dac/iio_axi_dac.h				\
+	$(NO-OS)/libraries/libtinyiiod/tinyiiod.h			\
+	$(NO-OS)/libraries/libtinyiiod/compat.h
